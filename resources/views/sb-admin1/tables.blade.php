48240e3644c044d8778db27abb119760e4ce432b
Zidaneyoga <93694885+Zidaneyoga@users.noreply.github.com>
2022-03-17 08:41:40 +0700

    up

--- resources/views/sb-admin1/tables.blade.php
+++ resources/views/sb-admin1/tables.blade.php
@@ -400,7 +400,7 @@
                 <div class="modal-body">Tekan "Logout" jika ingin keluar dari halaman user.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="../index">Logout</a>
+                    <a class="btn btn-primary" href="../">Logout</a>
                 </div>
             </div>
         </div>
--- resources/views/sb-admin2/admin.blade.php
+++ resources/views/sb-admin2/admin.blade.php
@@ -422,7 +422,7 @@
                 <div class="modal-body">Tekan "Logout" jika ingin keluar dari admin dan pergi ke portal.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="../index">Logout</a>
+                    <a class="btn btn-primary" href="../">Logout</a>
                 </div>
             </div>
         </div>
