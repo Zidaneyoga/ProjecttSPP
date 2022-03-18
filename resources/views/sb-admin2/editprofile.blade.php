06d65a1c185ae574faaac95d4b2c74402b3ea274
Zidaneyoga <93694885+Zidaneyoga@users.noreply.github.com>
2022-03-18 09:44:47 +0700

    up

--- resources/views/sb-admin2/editprofile.blade.php
+++ resources/views/sb-admin2/editprofile.blade.php
@@ -35,7 +35,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
              <!-- Sidebar - Brand -->
-             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/admin">
                 <div class="sidebar-brand-icon rotate-n-15">
                 </div>
                 <img src="img/bayarSPP-5.png" class="main-logo" width="50" alt="Awesome Image" />
